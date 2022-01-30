5ff0f22016b45766efaf98f8d05df904b2b873d0
sangeeet <ksangeethkumar666@gmail.com>
2022-01-30 21:44:05 +0530

    one changed

---
+++ add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int n,a,b;
+	printf("enter the a and b :");
+	scanf("%d %d",&a &b);
+	n=a+b;
+	printf("result:%d",n);
+	return 0;
+}
+
---
+++ sangeeth/my.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int c,a,b;
+	printf("enter the a and b:");
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	printf("result:%d",c);
+	return 0;
+}
